dc8b65d7c38c51c54d69016cd3d09b495224d476
singhayush6565-pixel <singhayush6565@gmail.com>
2026-05-21 22:49:51 +0530

    Update service worker cache version and URLs

--- sw.js
+++ sw.js
@@ -1,16 +1,25 @@
-const CACHE_NAME = 'ayush-it-v1';
+const CACHE_NAME = 'ayush-it-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/ayush-website/',
+  '/ayush-website/index.html',
+  '/ayush-website/manifest.json',
+  '/ayush-website/icon-192.png',
+  '/ayush-website/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
 
